a25cd0f3afe5460eb5bb7e1bf8c41380a0187f86
Shibaeva Maria <Bolzuka@gmail.com>
2017-12-18 14:34:16 +0300

    Update Answer.py

--- Answer.py
+++ Answer.py
@@ -1,22 +1,20 @@
 # https://api.vk.com/method/users.get?user_ids=53656294&v=5.8&fields=nickname,status,education,online,country
 
 import urllib.request
-import json, pprint
+import json
 
-json = urllib.request.urlopen("http://bit.ly/2Cu0ru5").read()
-data = json.loads(json)
+html = urllib.request.urlopen("http://bit.ly/2Cu0ru5").read()
+data = json.loads(html)
 u = data.get('response')
-#pprint.pprint(u)
 
 for user in u:
-    print("First name: " + str(user['first_name']))
-    print("Last name: " + str(user['last_name']))
-    print("Nickname: " + str(user['nickname']))
-    print("Status: " + str(user['status']))
-    print("Education: " + str(user['education']))    
-    print("Online: " + str(user['online']))
-    print("Сountry: " + str(user['country']))
-    
+print("First name: " + str(user['first_name']))
+print("Last name: " + str(user['last_name']))
+print("Nickname: " + str(user['nickname']))
+print("Status: " + str(user['status']))
+print("Online: " + str(user['online']))
+print("Сountry: " + str(user['country']))
+
 print('\n')
-print('_____'*10)
+print('_____' * 10)
 print('\n')
